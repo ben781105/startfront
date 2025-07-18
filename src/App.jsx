895ef3685377df7357dd38074b3e2fb73897d636
ssewabuga <benardleno78@gmail.com>
2025-07-18 23:08:30 +0300

    protected route

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,13 @@ import Homepage from "./components/homepage"
 import Dashboard from "./components/dashboard/dashboardLayout.jsx"
 import LoginForm from "./components/login.jsx"
 import RegisterForm from "./components/register.jsx"
-//import ProtectedRoute from "./protectedRoute.jsx"
+import ProtectedRoute from "./protectedRoute.jsx"
 function App(){
    return (
      <BrowserRouter>
        <Routes>
         <Route path="/" element={<Homepage/>}/>
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
         <Route path="/login" element={<LoginForm/>}/>
         <Route path="/register" element={<RegisterForm/>}/>
        </Routes>
